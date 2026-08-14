ce0e271ffa930929201a770f82718fdd5c15fd18
Ali <alisher.abdimuminov.2005@gmail.com>
2026-08-14 11:18:18 +0500

    bug fix

--- applications/models.py
+++ applications/models.py
@@ -9,7 +9,9 @@ class Application(models.Model):
         REJECTED = "rejected", "Rad etildi"
 
     user = models.OneToOneField(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="application"
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="application",
     )
 
     rector_application = models.FileField(
@@ -18,27 +20,76 @@ class Application(models.Model):
         null=True,
         blank=True,
     )
+    rector_application_status = models.CharField(
+        max_length=20,
+        choices=Status.choices,
+        default=Status.PENDING,
+    )
+    rector_application_comment = models.TextField(
+        null=True,
+        blank=True,
+    )
+
     passport = models.FileField(
         upload_to="docs/passport/",
         verbose_name="Pasport yoki ID-shaxs guvohnomasi nusxasi",
         null=True,
         blank=True,
     )
+    passport_status = models.CharField(
+        max_length=20,
+        choices=Status.choices,
+        default=Status.PENDING,
+    )
+    passport_comment = models.TextField(
+        null=True,
+        blank=True,
+    )
+
     diploma = models.FileField(
         upload_to="docs/diploma/",
         verbose_name="Oliy ma'lumot, ilmiy daraja to'g'risidagi diplomlar nusxasi",
         null=True,
         blank=True,
     )
+    diploma_status = models.CharField(
+        max_length=20,
+        choices=Status.choices,
+        default=Status.PENDING,
+    )
+    diploma_comment = models.TextField(
+        null=True,
+        blank=True,
+    )
+
     degree_diploma = models.FileField(
         upload_to="docs/degree/",
         verbose_name="Ilmiy unvoni to'g'risidagi diplomlar nusxasi",
         null=True,
         blank=True,
     )
+    degree_diploma_status = models.CharField(
+        max_length=20,
+        choices=Status.choices,
+        default=Status.PENDING,
+    )
+    degree_diploma_comment = models.TextField(
+        null=True,
+        blank=True,
+    )
+
     resume = models.FileField(
         upload_to="docs/resume/",
         verbose_name="Rezyume-ma'lumotnoma",
+        null=True,
+        blank=True,
+    )
+    resume_status = models.CharField(
+        max_length=20,
+        choices=Status.choices,
+        default=Status.PENDING,
+    )
+    resume_comment = models.TextField(
         null=True,
         blank=True,
     )
@@ -49,30 +100,46 @@ class Application(models.Model):
         null=True,
         verbose_name="Malaka oshirganligi to'g'risidagi guvohnoma",
     )
+    qualification_cert_status = models.CharField(
+        max_length=20,
+        choices=Status.choices,
+        default=Status.PENDING,
+    )
+    qualification_cert_comment = models.TextField(
+        null=True,
+        blank=True,
+    )
+
     scientific_works = models.FileField(
         upload_to="docs/works/",
         blank=True,
         null=True,
         verbose_name="Ilmiy ishlar ro'yxati",
     )
+    scientific_works_status = models.CharField(
+        max_length=20,
+        choices=Status.choices,
+        default=Status.PENDING,
+    )
+    scientific_works_comment = models.TextField(
+        null=True,
+        blank=True,
+    )
+
     language_cert = models.FileField(
         upload_to="docs/languages/",
         blank=True,
         null=True,
         verbose_name="Xorijiy tillarni bilish darajasini belgilovchi sertifikatlar",
     )
-
-    status = models.CharField(
+    language_cert_status = models.CharField(
         max_length=20,
         choices=Status.choices,
         default=Status.PENDING,
-        verbose_name="Ariza holati",
     )
-
-    rejection_comment = models.TextField(
+    language_cert_comment = models.TextField(
         null=True,
         blank=True,
-        verbose_name="Rad etish sababi",
     )
 
     created_at = models.DateTimeField(auto_now_add=True)
