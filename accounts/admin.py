2add16b5a4c07a5a2e571fd0d5beae841a20e56e
Ali <alisher.abdimuminov.2005@gmail.com>
2026-08-20 19:18:46 +0500

    bug fix

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,14 +1,40 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from typing_extensions import final
 
 from .models import User
 
 
-# Register your models here.
 @admin.register(User)
 class UserModelAdmin(UserAdmin, admin.ModelAdmin):
     list_display = ["phone", "first_name", "last_name"]
     add_form = UserCreationForm
     form = UserChangeForm
     search_fields = ["phone", "first_name", "last_name"]
+    fieldsets = (
+        (
+            "Foydalanuvchini tahrirlash",
+            {
+                "fields": (
+                    "phone",
+                    "first_name",
+                    "last_name",
+                )
+            },
+        ),
+    )
+    add_fieldsets = (
+        (
+            "Yangi foydalanuvchi qo'shish",
+            {
+                "fields": (
+                    "phone",
+                    "password1",
+                    "password2",
+                    "first_name",
+                    "last_name",
+                )
+            },
+        ),
+    )
