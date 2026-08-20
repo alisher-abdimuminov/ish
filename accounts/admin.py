60d325f386c3a718d27bc858254bc23080a36d25
Ali <alisher.abdimuminov.2005@gmail.com>
2026-08-20 19:11:56 +0500

    bug fix

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 
 from .models import User
 
 
 # Register your models here.
 @admin.register(User)
-class UserModelAdmin(admin.ModelAdmin):
+class UserModelAdmin(UserAdmin, admin.ModelAdmin):
     list_display = ["phone", "first_name", "last_name"]
+    add_form = UserCreationForm
+    form = UserChangeForm
+    search_fields = ["phone", "full_name"]
